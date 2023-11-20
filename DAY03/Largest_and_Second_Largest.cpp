2c083f9adc9dc7a1a8972e4d4d69c8ff7e6fbe61
MD ALIF <96830817+ALIFAHMEDONGKON21@users.noreply.github.com>
2023-11-20 23:22:24 +0600

    Add files via upload

---
+++ DAY03/Largest_and_Second_Largest.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll                              long long int
+#define     ull                             unsigned long long int
+#define     ui                              unsigned int
+//STL :
+#define     vi                              vector<int>
+#define     vb                              vector<bool>
+#define     vs                              vector<string>
+#define     vl                              vector<ll>
+#define     si                              set<int>
+#define     sl                              set<ll>
+#define     ip                              pair<int,int>
+#define     lp                              pair<ll,ll>
+#define     ips                             pair<string,int>
+#define     lps                             pair<string,ll>
+#define     ipc                             pair<char,int>
+#define     lpc                             pair<char,ll>
+#define     vip                             vector<ip>
+#define     PQ                              priority_queue
+#define     vlp                             vector<lp>
+#define     hashmap                         unordered_map
+#define     all(x)                          x.begin(),x.end()
+#define     forI                             (i,s,n) for(int i=s; i<n; i++)
+#define     in_range_back(i,x,y)            for(int i=y;i>=x;i--)
+#define     cin_the_array(x,o,n)            for(int i=o;i<=n;i++){cin>>x[i];}
+#define     cout_the_array(x,o,n)           for(int i=o;i<=n;i++){cout<<x[i]<<" ";}cout<<endl;
+
+
+
+//......Let's Start ........................//
+
+
+
+int main(){
+    
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        /* code */
+        int n;
+        cin>>n;
+
+        vi v(n);
+
+        for(int i =0; i<n; i++)
+        {
+            cin>>v[i];
+        }
+
+       
+        sort(v.begin(),v.end());
+        int lat_das=n-1;
+        while(v[lat_das]==v[n-1])
+        {
+            lat_das--;
+        }
+        int sum=v[lat_das]+v[n-1];
+        cout<<sum<<endl;
+    }
+    
+    
+    return 0;
+}
---
+++ DAY03/Return_the_Change.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll                              long long int
+#define     ull                             unsigned long long int
+#define     ui                              unsigned int
+//STL :
+#define     vi                              vector<int>
+#define     vb                              vector<bool>
+#define     vs                              vector<string>
+#define     vl                              vector<ll>
+#define     si                              set<int>
+#define     sl                              set<ll>
+#define     ip                              pair<int,int>
+#define     lp                              pair<ll,ll>
+#define     ips                             pair<string,int>
+#define     lps                             pair<string,ll>
+#define     ipc                             pair<char,int>
+#define     lpc                             pair<char,ll>
+#define     vip                             vector<ip>
+#define     PQ                              priority_queue
+#define     vlp                             vector<lp>
+#define     hashmap                         unordered_map
+#define     all(x)                          x.begin(),x.end()
+#define     forI                             (i,s,n) for(int i=s; i<n; i++)
+#define     in_range_back(i,x,y)            for(int i=y;i>=x;i--)
+#define     cin_the_array(x,o,n)            for(int i=o;i<=n;i++){cin>>x[i];}
+#define     cout_the_array(x,o,n)           for(int i=o;i<=n;i++){cout<<x[i]<<" ";}cout<<endl;
+
+
+
+//......Let's Start ........................//
+
+
+
+int main(){
+    
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        /* code */
+        int n;
+        cin>>n;
+
+        int rouondnuber =(n+5)/10*10;
+        int charge =100-rouondnuber;
+        cout<<charge<<endl;
+    }
+    
+    
+    return 0;
+}

04bd961df44f5ebd734b2942228668a3c2e9dd85
MD ALIF <96830817+ALIFAHMEDONGKON21@users.noreply.github.com>
2023-11-21 23:54:45 +0600

    Add files via upload

---
+++ DAY04/A_Simple_Design.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll                              long long int
+#define     ull                             unsigned long long int
+#define     ui                              unsigned int
+//STL :
+#define     vi                              vector<int>
+#define     vb                              vector<bool>
+#define     vs                              vector<string>
+#define     vl                              vector<ll>
+#define     si                              set<int>
+#define     sl                              set<ll>
+#define     ip                              pair<int,int>
+#define     lp                              pair<ll,ll>
+#define     ips                             pair<string,int>
+#define     lps                             pair<string,ll>
+#define     ipc                             pair<char,int>
+#define     lpc                             pair<char,ll>
+#define     vip                             vector<ip>
+#define     PQ                              priority_queue
+#define     vlp                             vector<lp>
+#define     hashmap                         unordered_map
+#define     all(x)                          x.begin(),x.end()
+#define     forI                             (i,s,n) for(int i=s; i<n; i++)
+#define     in_range_back(i,x,y)            for(int i=y;i>=x;i--)
+#define     cin_the_array(x,o,n)            for(int i=o;i<=n;i++){cin>>x[i];}
+#define     cout_the_array(x,o,n)           for(int i=o;i<=n;i++){cout<<x[i]<<" ";}cout<<endl;
+
+
+
+//......Let's Start ........................//
+
+
+
+int main(){
+    
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        /* code */
+        ll n,k;
+        cin>>n>>k;
+
+        while (1)
+        {
+            /* code */
+            int temp=n;
+            int sun=0;
+            while (temp!=0)
+            {
+               sun+=temp%10;
+               temp/=190;
+            }
+            if(sun%k==0)
+            {
+                break;
+            }
+            n++;
+        }
+        cout<<n<<endl;
+        
+    }
+    
+    
+    return 0;
+}
